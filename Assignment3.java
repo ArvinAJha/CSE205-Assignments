5423cf4806f32ff9ef45913c9a3ffd97e7d92c83
Arvin <arvinaayan@gmail.com>
2021-08-28 17:48:26 -0700

    done

--- Assignment3.java
+++ Assignment3.java
@@ -1,8 +1,22 @@
 Assingment #: 3
 Name: Arvin Jha
 StudentID: 1221497264
-
+Lecture: 10AM
 punlic class Assignment3 {
 
-    
+    3. Product - Software engineers shall ensure that their products and related modifications meet the highest professional standards possible.
+    - A software engineer writes a modification to a program using old unupdated recycled code that is very old inaccurate. This is not good.
+
+    7. COLLEAGUES - Software engineers shall be fair to and supportive of their colleagues.
+    - A senior software engineer wants get a promotion and prevent some lucky junior engineers from getting ahead so he takes credit for their work. This is bad
+
+    8. SELF - Software engineers shall participate in lifelong learning regarding the practice of their profession and shall promote an ethical approach to the practice of the profession.
+    - A software engineer practices artificial intelligence research on dead human brains in an attempt to revive them without taking any of the proper procedures of law or science. This is unethical
+
+    4. JUDGMENT - Software engineers shall maintain integrity and independence in their professional judgment.
+    - A software engineer refuses to personally develop a project that takes advantage of the local community's personal data 
+
+    5. MANAGEMENT - Software engineering managers and leaders shall subscribe to and promote an ethical approach to the management of software development and maintenance.
+    - A manager doesnt want to pay extra to maintain some program so he outsources the work to dirt cheap labor that mistreats its workers
+
 }
