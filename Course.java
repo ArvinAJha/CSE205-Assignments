7df86f9b089bcd93f60bfd9fd20a4df06cdb38e0
Arvin <arvinaayan@gmail.com>
2021-08-29 18:01:40 -0700

    added comments

--- Course.java
+++ Course.java
@@ -1,47 +1,87 @@
+/*
+Assignment #: 4
+Name: Arvin Jha
+StudentID: 1221497264
+Lecture: 10AM CSE205 M W F
+Description: Course is a class that describes a single particular course that the user has. Its attributes include the name of 
+    the course, the associated university, and the instructor assignment to the course. 
+ */
+
 public class Course {
 
+    //instance fields
     private String courseName; 
     private Instructor instructor; 
     private String university; 
     
+    /**
+     * //constructs a new course based on specified input
+     * @param name
+     * @param instructor
+     * @param university
+     */
     public Course(String name, Instructor instructor, String university) {
         this.courseName = name;
         this.instructor = instructor;
         this.university = university;
     }
 
+    //constructs the default course with no specific input
     public Course() {
         this.courseName = "?";
         this.instructor = new Instructor();
         this.university = "?";
     }
 
+    /**
+     * 
+     * @return current name of course
+     */
     public String getName() {
         return courseName;
     }
 
+    /**
+     * @return instructor object
+     */
     public Instructor getInstructor() {
         return instructor;
     }
 
+    /**
+     * 
+     * @return current university 
+     */
     public String getUniversity() {
         return university;
     }
 
+    /**
+     * changes course name based on string input
+     * @param newName
+     */
     public void setName(String newName) {
         courseName = newName;
     }
 
+    //changes the current instructor's paramaters with new ones
     public void setInstructor(String firstName, String lastName, String officeNumber) {
         instructor.setFirstName(firstName);
         instructor.setLastName(lastName);
         instructor.setOfficeNumber(officeNumber);
     }
 
+    /**
+     * changes course's current university with new university based on string input
+     * @param newUniversity
+     */
     public void setUniversity(String newUniversity) {
         university = newUniversity;
     }
 
+    /**
+     * @return formatted string with course name, university, and instructor in a specific format
+     */
     public String toString() {
         return "Course name:\t" + courseName + " at " + university + "\nInstructor Information:" + instructor;
     }
--- Instructor.java
+++ Instructor.java
@@ -3,54 +3,88 @@ Assignment #: 4
 Name: Arvin Jha
 StudentID: 1221497264
 Lecture: 10AM CSE205 M W F
-Description: Assignment 4 class displays a menu of choices to a user
-  and performs the chosen task. It will keep asking a user to
- enter the next choice until the choice of 'Q' (Quit) is entered.
+Description: Instructor describes the information of the university instructor for a particular course. Its attributes hold
+    the first and last name and its office number.
 */
 
 
 public class Instructor {
 
+    //instance fields
     private String firstName;
     private String lastName;
     private String officeNum;
 
+    /**
+     * constructs a new instructor with specified input
+     * @param firstName
+     * @param lastName
+     * @param office
+     */
     public Instructor(String firstName, String lastName, String office) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.officeNum = office;
     }
 
+    //constructs default instructor
     public Instructor() {
         this.firstName = "?";
         this.lastName = "?";
         this.officeNum = "?";
     }
 
+    /**
+     * 
+     * @return first name of instructor
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * 
+     * @return last name of instructor
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * 
+     * @return instructor's office number
+     */
     public String getOfficeNum() {
         return officeNum;
     }
 
+    /**
+     * changes instructor's first name with new name based on parameter
+     * @param newName
+     */
     public void setFirstName(String newName) {
         firstName = newName;
     }
 
+    /**
+     * changes instructor's last name with new name based on parameter
+     * @param newName
+     */
     public void setLastName(String newName) {
         lastName = newName;
     }
 
+    /**
+     * changes instructor's office number with new office number based on provided input
+     * @param newOfficeNumber
+     */
     public void setOfficeNumber(String newOfficeNumber) {
         officeNum = newOfficeNumber;
     }
 
+    /**
+     * @return returns string message in a specified format 
+     */
     public String toString() {
         return "\nLast Name:\t" + lastName + "\nFirst Name:\t" + firstName + "\nOffice Number:\t" + officeNum + "\n";
     }
