f5c03a160b8961488c921f022886f7c3bbd100e1
Arvin <arvinaayan@gmail.com>
2021-10-25 16:13:54 -0700

    init

---
+++ Main.java
@@ -0,0 +1,12 @@
+import com.apple.eawt.Application;
+
+public class Main extends Application{
+
+    public void start() {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
