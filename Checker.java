a3cff0e1c812a1f2c6a31b525b695db6b99fd2a7
Arvin <arvinaayan@gmail.com>
2021-11-14 18:56:16 -0700

    init

---
+++ Checker.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class Checker {
+
+    public static Stack<String> aStack;
+
+    public static String check(String inputInfo) {
+        aStack = new Stack<String>();
+
+        int count = 0;
+
+        String[] letters = inputInfo.split("");
+
+        System.out.println(letters.toString());
+
+        return null;
+    }
+
+}
